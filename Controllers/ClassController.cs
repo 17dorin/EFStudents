a6f1a7140a8578ca6746c8b0a17df1cf709289a5
Nick D'Oria <17dorin@gmail.com>
2021-02-23 16:49:17 -0500

    Initial commit, still need to add CRUD actions

---
+++ Controllers/ClassController.cs
@@ -0,0 +1,23 @@
+﻿using EFStudents.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFStudents.Controllers
+{
+    public class ClassController : Controller
+    {
+        private readonly ClassContext _classDb;
+
+        public ClassController(ClassContext classContext)
+        {
+            _classDb = classContext;
+        }
+        public IActionResult Index()
+        {
+            return View(_classDb.Students.ToList());
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EFStudents.Models
+{
+    public partial class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Hometown { get; set; }
+        public string FavoriteFood { get; set; }
+        public string FavoriteColor { get; set; }
+    }
+}
